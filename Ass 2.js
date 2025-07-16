24553218a9bd651c7e4cb0b0d10c9db1a79538bf
Madubuko <ifeanyimadubuko11@gmail.com>
2025-07-16 21:41:32 +0100

    changed both html and js syntax

--- Ass 2.js
+++ Ass 2.js
@@ -1,39 +1,26 @@
-const emailRegex = /^[a-z0-9]+@[a-z]+\.[a-z]/;
-const passwordRegex = /^.{4,8}/;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^.{4,8}$/;
 
-const form = document.getElementById('form');
+document.getElementById('form').addEventListener("submit", function (e) {
+    e.preventDefault();
 
-form.addEventListener("submit", function ( e{
-    e.preventDefault();````````````````````````````````````````
-}))
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value;
+    const error = document.getElementById("error");
+    error.innerText = "";
+    
+    if (!emailRegex.test(email)) {
+        error.style.color = "red";
+        error.innerText = "Invalid email. Kindly enter a valid email address.";
+        return;
 
-const email = document.getElementById('email').value;
-const password = document.getElementById('passwprd').value;
-const error = document.getElementById("error");
-error.innerText ="";
+    }
 
-if (!emailRegex.test(email)) {
-    error.style.color = "red"
-    error.innerText = "Invalid email. Kindly enter a valid email address.";
-    return;
-}
+    if (!passwordRegex.test(password)) {
+        error.style.color = "red";
+        error.innerText = "Invalid password, it must be 4–8 characters long";
+        return;
+    }
 
-if (password.lenght <4) {
-    error.style.color = "red"
-    error.innerText = "Password too short. Minimum of 4 character required.";
-    return
-
-}else
-error.style.color = "red"
-error.innerText = "Password is valid!";
-
-if (password.lenght >8) {
-    error.style.color = "red"
-    error.innerText = "Password too long. Maximum of 8 characters required.";
-    return
-
-}else 
-error.style.color = "green"
-error.innerText = "Email and password are valid";
-});
-
+    error.style.color = "green";
+    error.innerText = "Email and password are valid!";});
